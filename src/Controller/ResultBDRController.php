fd72f19c3789f049482591db958dbaed3de1b9ea
CDA22 <tiffany.dellmann@gmail.com>
2023-02-07 12:31:37 +0100

    fiiiiiniishhh

--- src/Controller/ResultBDRController.php
+++ src/Controller/ResultBDRController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\SearchForm;
 use App\Form\SearchForm2;
 use App\Repository\FreelanceRepository;
 use Doctrine\DBAL\Exception;
@@ -38,30 +39,24 @@ class ResultBDRController extends AbstractController
             12
         );
 
-//        $searchFreelance2 = $this->createForm(SearchForm2::class, null);
-//        // crée le formulaire configuré dans le dossier FORM
-//        $searchFreelance2->handleRequest($request);
-//        // traite les données du formulaire
-//
-//
-//        if ($searchFreelance2->isSubmitted() && $searchFreelance2->isValid()) {
-//            // si le formulaire est envoyé et validé alors :
-//            $freelances2 = $repository->findSearch2($searchFreelance2->getData());
-//            $request->query->remove('_token');
-//            // on passe le formulaire a la fonction du repository qui est un tableau classic : FreelanceRepository.php
-//            return $this->redirectToRoute('app_resultbdr_index', $request->query->all());
-//        };
-//
-//        $freelances2 = $repository->findSearch2([
-//            'language' => $request->query->get('codingLanguages'),
-//        ]);
-//        // $freelances = le tableau des freelances trouvés.
+        $searchFreelance = $this->createForm(SearchForm2::class, null);
+        // crée le formulaire configuré dans le dossier FORM
+        $searchFreelance->handleRequest($request);
+        // traite les données du formulaire
+
+
+        if ($searchFreelance->isSubmitted() && $searchFreelance->isValid()) {
+            // si le formulaire est envoyé et validé alors :
+            $request->query->remove('_token');
+            // on passe le formulaire a la fonction du repository qui est un tableau classic : FreelanceRepository.php
+
+            return $this->redirectToRoute('app_resultbdr_index', $request->query->all());
+        };
 
         return $this->render('result_bdr/index.html.twig', [
-//            'searchFreelance2' => $searchFreelance2->createView(),
             'freelances' => $freelances ?? null,
-//            'freelances2' => $freelances2 ?? null,
             'user' => $user ?? null,
+            'searchFreelance2' => $searchFreelance->createView()
         ]);
     }
 
