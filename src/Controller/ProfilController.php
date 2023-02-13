0557b153cb9141355dc14a4b0bc8e38c3c4344eb
Do-Katsu <kebsibadr@gmail.com>
2023-02-13 14:11:52 +0100

    affichage des commentaires

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -8,6 +8,7 @@ use App\Entity\Freelance;
 use App\Entity\Mission;
 use App\Entity\Social;
 use App\Entity\User;
+use App\Form\CommentType;
 use App\Form\DeleteSkillsType;
 use App\Form\DescriptionProfilType;
 use App\Form\DurationPrefType;
@@ -15,6 +16,7 @@ use App\Form\EditHeaderProfilType;
 use App\Form\LanguageType;
 use App\Form\LocationRemoteType;
 use App\Form\TechnologyType;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -150,7 +152,7 @@ class ProfilController extends AbstractController
 
 
     #[Route('/profil/{id}', name: 'app_profil_show')]
-    public function show($id): Response
+    public function show($id, Request $request): Response
     {
         $freelance = $this->entityManager->getRepository(Freelance::class)->find($id);
         $mission = $this->entityManager->getRepository(Mission::class)->find(['id'=>$freelance]);
@@ -159,12 +161,29 @@ class ProfilController extends AbstractController
 
         $user = $this->getUser() ?? null;
 
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment
+                ->setCreatedAt(new DateTimeImmutable())
+                ->setComments($user)
+                ->setReceived($freelance)
+                ;
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+            
+            return $this->redirectToRoute('app_profil_show', ['id' => $freelance->getId()]);
+        }
+        
         return $this->render('profil/show.html.twig',[
         'freelance'=>$freelance,
             'mission'=>$mission,
             'commentaire'=>$commentaire,
             'social'=>$social,
             'user'=>$user,
+            'commentForm' => $commentForm->createView(),
         ]);
     }
 
