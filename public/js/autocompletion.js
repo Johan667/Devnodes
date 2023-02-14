c507899214827489f1db801895dc3d95c7221ef5
CDA22 <tiffany.dellmann@gmail.com>
2023-02-14 14:29:52 +0100

    fichierJS

---
+++ public/js/autocompletion.js
@@ -0,0 +1,6 @@
+const input = document.getElementById('city');
+var autocomplete = new google.maps.places.Autocomplete(input);
+google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    const place = autocomplete.getPlace();
+    input.value = place.name
+});
