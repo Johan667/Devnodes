d8917453f4d95421e2717b33f86bbf8bfa1e10f2
johan <johan.kasri@icloud.com>
2023-02-25 13:46:55 +0100

    miaou

--- src/Controller/RegistrationFreelanceController.php
+++ src/Controller/RegistrationFreelanceController.php
@@ -6,7 +6,6 @@ use App\Entity\Freelance;
 use App\Form\RegistrationFreelanceType;
 use App\Repository\FreelanceRepository;
 use App\Security\AppCustomAuthenticator;
-use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,13 +14,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationFreelanceController extends AbstractController
 {
     #[Route('/register/freelance', name: 'app_register_freelance')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager, VerifyEmailHelperInterface $verifyEmailHelper): Response
     {
         $freelance = new Freelance();
         $form = $this->createForm(RegistrationFreelanceType::class, $freelance);
@@ -40,25 +40,39 @@ class RegistrationFreelanceController extends AbstractController
                     $form->get('password')->getData()
                 )
             );
+
             $entityManager->persist($freelance);
             $entityManager->flush();
+
+            $signatureComponents = $verifyEmailHelper->generateSignature('app_verify_email', $freelance->getId(), $freelance->getEmail());
+            $signedUrl = $this->generateUrl('app_verify_email', $signatureComponents, true);
+
+            $email = (new TemplatedEmail())
+                ->from('devnodes.contact@gmail.com')
+                ->to($freelance->getEmail())
+                ->subject('Veuillez confirmer votre adresse e-mail')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+                ->context([
+                    'signedUrl' => $signedUrl,
+                ]);
+
+            $mailer->send($email);
+
             $userAuthenticator->authenticateUser(
                 $freelance,
                 $authenticator,
                 $request
             );
-            return $this->redirectToRoute('app_home', [], 302, [
-                'onload' => 'alert("Bravo, il suffit maintenant de remplir vos compétences et autres informations")'
-            ]);
+
+            $this->addFlash('success', 'Votre inscription a été réussie. Veuillez confirmer votre adresse e-mail en cliquant sur le lien que nous venons de vous envoyer.');
+
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration_freelance/index.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-
-
-
 
     #[Route('/verify/email', name: 'app_verify_email')]
     public function verifyUserEmail(Request $request, TranslatorInterface $translator, FreelanceRepository $freelanceRepository): Response
